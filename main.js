c1d947d06373d51f2a65a3810cef8a613e9c1f9d
d4nkali <133923419+d4nkali@users.noreply.github.com>
2024-08-02 15:39:33 -0300

    Corrigindo e comentando

--- main.js
+++ main.js
@@ -1,6 +1,17 @@
+/*
+
+Arquivo principal para criar e renderizar o app pelo Electron
+
+
+Autor: d4nkali
+Data: 24/07/2024
+
+*/
+
+// Importa a biblioteca do Electron
 const { app, BrowserWindow } = require('electron');
 
-function createWindow() {
+function createWindow() { //* Função para criar a janela do app
 
     const mainWindow = new BrowserWindow({
 
@@ -9,11 +20,11 @@ function createWindow() {
 
     });
 
-    mainWindow.loadFile('src/index.html');
+    mainWindow.loadFile('src/index.html'); // Abre o index.html na janela do app
 
 }
 
-app.whenReady().then(() => {
+app.whenReady().then(() => { //* Inicia o Electron e cria a janela
 
     createWindow();
 
@@ -27,10 +38,12 @@ app.whenReady().then(() => {
 
 });
 
-app.on('window-all-closed', () => {
+app.on('window-all-closed', () => { //* Função para fechar o app
 
-    if (process.platform !== 'darwin') {
-        app.quit();
+    if (process.platform !== 'darwin') { //! Se o OS for qualquer um exceto o MacOS
+
+        app.quit(); // Fechar aplicativo
+
     }
 
 });
