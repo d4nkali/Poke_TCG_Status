f63fcd2c512d804d5b01d5b7c78e9b3d37b6bdad
d4nkali <133923419+d4nkali@users.noreply.github.com>
2024-10-24 14:59:30 -0300

    Melhorando Script eletron build

--- Instalando_Depedencias_NPM.sh
+++ Instalando_Depedencias_NPM.sh
@@ -3,5 +3,6 @@
 # Script da instalção das depedencias NPM do projeto
 
 npm install --save-dev prettier electron electron-builder
+npm install
 node --eval "fs.writeFileSync('.prettierrc','{}\n')"
 node --eval "fs.writeFileSync('.prettierignore','')"
