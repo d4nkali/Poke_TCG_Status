e634c3b04fac1c02844aadc1165a9ae7acfb5bd1
d4nkali <133923419+d4nkali@users.noreply.github.com>
2025-01-06 10:59:59 -0300

    Removendo script

--- Instalando_Depedencias_NPM.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Script da instalção das depedencias NPM do projeto
-
-npm install --save-dev prettier electron electron-builder
-npm install
-node --eval "fs.writeFileSync('.prettierrc','{}\n')"
-node --eval "fs.writeFileSync('.prettierignore','')"
