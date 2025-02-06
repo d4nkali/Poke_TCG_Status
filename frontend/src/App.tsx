268eddc6a3294b22ecc298bb762f864ff51f9edb
d4nkali <133923419+d4nkali@users.noreply.github.com>
2025-02-06 10:42:30 -0300

    feat: Adicionando pagina para react e removendo css global

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,44 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    return (
+
+        <>
+
+            <main>
+
+            <h1>Pokémon TCG App</h1>
+
+            {/* Gerenciar Pokémon's em batalha */}
+
+            <samp>Nome do Pokémon:</samp>
+            <input type="text" id="nome_poke" placeholder="Nome do Pokémon"></input>
+            <samp>Status:</samp>
+            <select id="status_poke">
+
+                <option className="normal" value="Normal">Normal</option>
+                <option className="queimado" value="Queimado">Queimado</option>
+                <option className="envenenado" value="Envenenado">Envenenado</option>
+                <option className="adormecido" value="Adormecido">Adormecido</option>
+                <option className="paralisado" value="Paralisado">Paralisado</option>
+                <option className="confuso" value="Confuso">Confuso</option>
+
+            </select>
+
+            <button id="adicionar_poke">Adicionar Pokémon</button>
+
+            <ul id="lista_poke"></ul>
+
+            </main>
+
+            <script src="script.js"></script>
+
+
+        </>
+
+    )
+
 }
 
 export default App
